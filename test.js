4078d5e934a1a83cc1106fe53d023a7c54fcf723
okamuuu <okamuuu@gmail.com>
2019-09-11 09:44:41 +0900

    feat: add ci test

---
+++ test.js
@@ -0,0 +1,25 @@
+const { test } = require('tap')
+
+const PerformanceMeasure = require('./')
+
+function sleep(ms = 0) {
+  return new Promise(r => setTimeout(r, ms));
+}
+
+test('produces some stats', async (t) => {
+  const m = new PerformanceMeasure()
+  m.start('foo')
+  await sleep(10)
+  m.end('foo')
+  
+  m.start('foo')
+  await sleep(10)
+  m.end('foo')
+
+  const stats = m.stats()
+
+  t.ok(stats[0].name === 'foo')
+  t.ok(stats[0].size === 2)
+})
+
+
